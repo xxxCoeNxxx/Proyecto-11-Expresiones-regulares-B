d4f90bd5cb1dab34882213f4a5ec57b558dafb6a
xxxCoeNxxx <pedrocela1992@gmail.com>
2025-12-11 00:12:08 +0100

    con imgs

--- src/ui.ts
+++ src/ui.ts
@@ -6,20 +6,28 @@ const crearParrafo = (texto: string): HTMLParagraphElement => {
   return p;
 };
 
-/* const crearImagen = (url: string): HTMLImageElement => {
+const crearImagen = (url: string): HTMLImageElement => {
   const img = document.createElement("img");
   img.src = url;
   img.classList.add("imagen-extraida");
   return img;
-}; */
+};
 
 export const mostrarEnlaces = (urlsArray: string[]): HTMLDivElement => {
   const contenedor = document.createElement("div");
-  contenedor.classList.add("contenedor-urls");
+  contenedor.classList.add("contenedor-img-urls");
 
   urlsArray.forEach(url => {
+    const divImagen = document.createElement("div-img");
+    divImagen.classList.add("imagen");
+
+    const img = crearImagen(url);
     const p = crearParrafo(url);
-    contenedor.appendChild(p);
+
+    divImagen.appendChild(img);
+    divImagen.appendChild(p);
+
+    contenedor.appendChild(divImagen);
   });
 
   return contenedor;
